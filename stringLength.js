3d791985d97d26b161cf7df7a000ecc460429e78
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-10-05 09:54:11 -0400

    Add length tests

--- stringLength.js
+++ stringLength.js
@@ -1,4 +1,9 @@
 function stringLength(str) {
+    if(str.length > 10) {
+        throw new Error('String is too long');
+    } else if(str.length < 1) {
+        throw new Error("String is too short");
+    }
     return str.length;
 }
 
--- stringLength.test.js
+++ stringLength.test.js
@@ -3,3 +3,11 @@ const stringLength = require("./stringLength");
 it('returns the length of a string', () => {
     expect(stringLength('hello')).toBe(5);
 });
+
+it('throws an error if the string is too long', () => {
+    expect(() => stringLength('hello world')).toThrow();
+});
+
+it('throws an error if the string is too short', () => {
+    expect(() => stringLength('')).toThrow();
+});
